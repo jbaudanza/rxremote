a1135281f53a26a96c267f1f51cbfed389b64d4d
Jonathan Baudanza <jon@jonb.org>
2016-11-06 20:00:33 -0800

    Moved resumable api into its own file

---
+++ src/resumable.js
@@ -0,0 +1,42 @@
+import Rx from 'rxjs';
+
+
+export function resumableWithOffset(observable) {
+  return resumable(
+    (offset) => observable.skip(offset),
+    (value, offset) => offset + 1,
+    0
+  );
+}
+
+export function resumable(resumeFn, cursorFn, initialCursor) {
+  return resumeFromCursor.bind(undefined, resumeFn, cursorFn, initialCursor);
+}
+
+export function resumeFromCursor(resumeFn, cursorFn, initialCursor, resumeCursor) {
+  const obs = Rx.Observable.create(function(observer) {
+    let cursor;
+    if (typeof resumeCursor === 'undefined') {
+      cursor = initialCursor;
+    } else {
+      cursor = resumeCursor;
+    }
+
+    const observable = resumeFn(cursor);
+
+    function next(value) {
+      cursor = cursorFn(value, cursor);
+      observer.next({value, cursor});
+    }
+
+    observable.subscribe({
+      next: next,
+      error: observer.error.bind(observer),
+      complete: observer.complete.bind(observer)
+    });
+  });
+
+  obs.resumable = true;
+
+  return obs;
+}
--- test/observable_server_spec.js
+++ test/observable_server_spec.js
@@ -4,7 +4,7 @@ import http from 'http';
 import Rx from 'rxjs';
 
 import {Server, Client} from '../';
-
+import {resumableWithOffset} from '../resumable';
 
 function createHttpServer() {
   const httpServer = http.createServer();
@@ -151,44 +151,3 @@ describe('ObservableServer', () => {
     });
   });
 });
-
-// TODO: These need to go somewhere
-function resumableWithOffset(observable) {
-  return resumable(
-    (offset) => observable.skip(offset),
-    (value, offset) => offset + 1,
-    0
-  );
-}
-
-function resumable(resumeFn, cursorFn, initialCursor) {
-  return resumeFromCursor.bind(undefined, resumeFn, cursorFn, initialCursor);
-}
-
-function resumeFromCursor(resumeFn, cursorFn, initialCursor, resumeCursor) {
-  const obs = Rx.Observable.create(function(observer) {
-    let cursor;
-    if (typeof resumeCursor === 'undefined') {
-      cursor = initialCursor;
-    } else {
-      cursor = resumeCursor;
-    }
-
-    const observable = resumeFn(cursor);
-
-    function next(value) {
-      cursor = cursorFn(value, cursor);
-      observer.next({value, cursor});
-    }
-
-    observable.subscribe({
-      next: next,
-      error: observer.error.bind(observer),
-      complete: observer.complete.bind(observer)
-    });
-  });
-
-  obs.resumable = true;
-
-  return obs;
-}
